9d32552aa6589fc913a34b12759a11a1a443c597
Davensko <al1609053@upy.edu.mx>
2019-07-15 21:28:00 -0500

    Adding High Level Function

--- controllers/U3_Project_Gonzalez_Julio/U3_Project_Gonzalez_Julio.c
+++ controllers/U3_Project_Gonzalez_Julio/U3_Project_Gonzalez_Julio.c
@@ -2,7 +2,7 @@
  * File:          U3_Project_Gonzalez_Julio.c
  * Date:          July 11
  * Description:   3 wheels Robot Movement
- * Author:          
+ * Author:
  * Modifications:
  */
 
@@ -17,221 +17,216 @@
 #define TIME_STEP 64
 //#define pi 3.1416
 
- 
-double sn; 
-double sn_2; 
+
+double sn;
+double sn_2;
 double dis;
-double dis_2; 
+double dis_2;
 double encoder;
 double vel = 2.0;
- 
+
 int stop_r, stop_l = 0;
 
 float compare = 0;
 
-
-
-
-/*
-void Forward(WbDeviceTag *wheels) {
+void autoForward(WbDeviceTag *wheels) {
+    wb_motor_set_position(wheels[0], INFINITY);
+    wb_motor_set_velocity(wheels[0], 0);
+    wb_motor_set_position(wheels[1], INFINITY);
+    wb_motor_set_velocity(wheels[1], -1);
+    wb_motor_set_position(wheels[2], INFINITY);
+    wb_motor_set_velocity(wheels[2], 1);
+}
+void autoRight(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 5);
+  wb_motor_set_velocity(wheels[1], 5);
+  wb_motor_set_velocity(wheels[2], 5);
+}
+void manualForward(WbDeviceTag *wheels) {
   wb_motor_set_velocity(wheels[0], 0);
-  wb_motor_set_velocity(wheels[1], vel);
-  wb_motor_set_velocity(wheels[2], vel);
+  wb_motor_set_velocity(wheels[1], -2);
+  wb_motor_set_velocity(wheels[2], 2);
+}
+void manualBackward(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 0);
+  wb_motor_set_velocity(wheels[1], 2);
+  wb_motor_set_velocity(wheels[2], -2);
+}
+void manualRight(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], -4);
+  wb_motor_set_velocity(wheels[1], 2);
+  wb_motor_set_velocity(wheels[2], 2);
+}
+void manualLeft(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 4);
+  wb_motor_set_velocity(wheels[1], -2);
+  wb_motor_set_velocity(wheels[2], -2);
+}
+void turnRight(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 2);
+  wb_motor_set_velocity(wheels[1], 2);
+  wb_motor_set_velocity(wheels[2], 2);
+}
+void turnLeft(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], -2);
+  wb_motor_set_velocity(wheels[1], -2);
+  wb_motor_set_velocity(wheels[2], -2);
+}
+void stopMotors(WbDeviceTag *wheels) {
+  wb_motor_set_velocity(wheels[0], 0);
+  wb_motor_set_velocity(wheels[1], 0);
+  wb_motor_set_velocity(wheels[2], 0);
 }
 
-void TurnRight(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0], vel);
-  wb_motor_set_velocity(wheels[1], vel);
-  wb_motor_set_velocity(wheels[2], vel);
-}
-
-void TurnLeft(WbDeviceTag *wheels) {
-  wb_motor_set_velocity(wheels[0], vel);
-  wb_motor_set_velocity(wheels[1], vel);
-  wb_motor_set_velocity(wheels[2], vel);
-}
-*/
-
-
-
- 
 int main(int argc, char **argv){
 
   int key;
-  int x= 1; 
-  int y= 0; 
+  int x= 1;
+  int y= 0;
 
- 
   wb_robot_init();
   wb_keyboard_enable(TIME_STEP);
- 
-  WbDeviceTag wheel_1 = wb_robot_get_device("Wheel_1");
-  WbDeviceTag wheel_2 = wb_robot_get_device("Wheel_2");
-  WbDeviceTag wheel_3 = wb_robot_get_device("Wheel_3");
-     
+
+  WbDeviceTag Wheel_1 = wb_robot_get_device("Wheel_1");
+  WbDeviceTag Wheel_2 = wb_robot_get_device("Wheel_2");
+  WbDeviceTag Wheel_3 = wb_robot_get_device("Wheel_3");
+
+  WbDeviceTag wheels[3];
+  wheels[0] = Wheel_1;
+  wheels[1] = Wheel_2;
+  wheels[2] = Wheel_3;
+
   WbDeviceTag sen_1= wb_robot_get_device("sen_1");
   WbDeviceTag sen_2 = wb_robot_get_device("sen_2");
-     
+
   wb_distance_sensor_enable(sen_1,TIME_STEP);
   wb_distance_sensor_enable(sen_2,TIME_STEP);
-     
+
   WbDeviceTag enco_1 = wb_robot_get_device("encoder_1");
   WbDeviceTag enco_2 = wb_robot_get_device("encoder_2");
   WbDeviceTag enco_3 = wb_robot_get_device("encoder_3");
-     
+
   wb_position_sensor_enable(enco_1, TIME_STEP);
   wb_position_sensor_enable(enco_2, TIME_STEP);
   wb_position_sensor_enable(enco_3, TIME_STEP);
-     
-     
-  void automatic(){
-  
-    wb_motor_set_position(wheel_1, INFINITY);
-    wb_motor_set_velocity(wheel_1, 0);
-    wb_motor_set_position(wheel_2, INFINITY);
-    wb_motor_set_velocity(wheel_2, -2);
-    wb_motor_set_position(wheel_3, INFINITY);
-    wb_motor_set_velocity(wheel_3, 2);
-     
+
+void automatic(){
+
+    autoForward(wheels);
+
     sn = wb_distance_sensor_get_value(sen_1);
     dis = (sn*0.2)/65535;
-     
+
     sn_2 = wb_distance_sensor_get_value(sen_2);
     dis_2 = (sn_2*0.2)/65535;
-     
+
     encoder = wb_position_sensor_get_value(enco_3);
-     
+
     if((dis <= 0.17 && dis>dis_2) && stop_r == 0){
-    
+
       compare = encoder + (2.35619);   // 2.35619 rad/s = 135 degrees
       stop_r = 1;
-      
+
     }
 
     if(stop_r == 1){
 
       if(encoder <= compare){
-      
-        wb_motor_set_velocity(wheel_1, 5);
-        wb_motor_set_velocity(wheel_2, 5);
-        wb_motor_set_velocity(wheel_3, 5);
-        
+        autoRight(wheels);
       }
-     
+
       else{
-        wb_motor_set_velocity(wheel_1, 0);
-        wb_motor_set_velocity(wheel_2, 0);
-        wb_motor_set_velocity(wheel_3, 0);
-        
+        stopMotors(wheels);
         stop_r = 0;
       }
-             
+
     }
 
     if((dis_2 <= 0.17 && dis<dis_2) && stop_l == 0){
-    
-      compare = encoder - (2.35619); 
+
+      compare = encoder - (2.35619);
       stop_l = 1;
-      
+
     }
 
     if(stop_l == 1){
 
       if(encoder >= compare){
-      
-        wb_motor_set_velocity(wheel_1, -5);
-        wb_motor_set_velocity(wheel_2, -5);
-        wb_motor_set_velocity(wheel_3, -5);
+
+        wb_motor_set_velocity(wheels[0], -5);
+        wb_motor_set_velocity(wheels[1], -5);
+        wb_motor_set_velocity(wheels[2], -5);
       }
-      
+
       else{
-      
-        wb_motor_set_velocity(wheel_1, 0);
-        wb_motor_set_velocity(wheel_2, 0);
-        wb_motor_set_velocity(wheel_3, 0);
-        
+
+        wb_motor_set_velocity(wheels[0], 0);
+        wb_motor_set_velocity(wheels[1], 0);
+        wb_motor_set_velocity(wheels[2], 0);
+
         stop_l = 0;
-        
-      }   
-     
-    }     
-    
+
+      }
+
+    }
+
     printf("sensor: %f\n", dis);
     printf("sensor2: %f\n", dis_2);
     printf("Encoder: %f\n", encoder);
     printf("flag: %f\n", compare);
-  
-  }
-  
-  void manual(){
-  
-    
-   
-   if(key == WB_KEYBOARD_UP){
 
-       wb_motor_set_velocity(wheel_1, 0);
-       wb_motor_set_velocity(wheel_2, -2);
-       wb_motor_set_velocity(wheel_3, 2);
+  }
+
+void manual(){
+
+  if(key == WB_KEYBOARD_UP){
+    manualForward(wheels);
   }
   else if(key ==WB_KEYBOARD_DOWN){
-       wb_motor_set_velocity(wheel_1, 0);
-       wb_motor_set_velocity(wheel_2, 2);
-       wb_motor_set_velocity(wheel_3, -2);
+    manualBackward(wheels);
   }
   else if(key ==WB_KEYBOARD_RIGHT){
-       wb_motor_set_velocity(wheel_1, -4);
-       wb_motor_set_velocity(wheel_2, 2);
-       wb_motor_set_velocity(wheel_3, 2);
+    manualRight(wheels);
   }
   else if(key ==WB_KEYBOARD_LEFT){
-       wb_motor_set_velocity(wheel_1, 4);
-       wb_motor_set_velocity(wheel_2, -2);
-       wb_motor_set_velocity(wheel_3, -2);
+    manualLeft(wheels);
   }
   else if(key == 'S'){
-       wb_motor_set_velocity(wheel_1, 2);
-       wb_motor_set_velocity(wheel_2, 2);
-       wb_motor_set_velocity(wheel_3, 2);
+    turnRight(wheels);
   }
   else if(key =='A'){
-       wb_motor_set_velocity(wheel_1, -2);
-       wb_motor_set_velocity(wheel_2, -2);
-       wb_motor_set_velocity(wheel_3, -2);
+    turnLeft(wheels);
   }
   else{
- 
-  wb_motor_set_velocity(wheel_1, 0);
-  wb_motor_set_velocity(wheel_2, 0);
-  wb_motor_set_velocity(wheel_3, 0);
+    stopMotors(wheels);
   }
-  
+
   }
-         
-   
+
   while (wb_robot_step(TIME_STEP) != -1) {
-  
+
   key = wb_keyboard_get_key();
-  
-  
+
+
    if(key == 'W'){
     x = 1;
     y = 0;
     //printf("Manual mode \n");
-  } 
-  
+  }
+
   else if (key == 'G'){
     y = 1;
     x = 0;
    //printf("Automatic mode \n");
   }
-  
+
   if(y == 1){
   manual();
   }
   if(x == 1){
   automatic();
   }
-   
+
   };
 
   wb_robot_cleanup();
